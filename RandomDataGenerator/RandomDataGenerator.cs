17c48d5a3a406274173dbac24959facba884c939
ATKINSONJM <jordan.atkinson3@gmail.com>
2016-04-04 15:51:35 +0100

    Changed the ETL for links and creating the inital data genereator

--- RandomDataGenerator/RandomDataGenerator.cs
+++ RandomDataGenerator/RandomDataGenerator.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RandomDataGenerator
 {
-    class RandomDataGenerator
+    public class RandomDataGenerator
     {
+        public static SqlConnection con = new SqlConnection();
+        
         static void Main(string[] args)
         {
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["TrendbaseConnecctionString"].ToString();
+        }
+
+        /// <summary>
+        /// Method which will genreate data for people buying tickets and will act as a constant stream of data. 
+        /// </summary>
+        public void GenerateData()
+        {
+            
         }
     }
 }
